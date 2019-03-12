5aa021687d360287742c01590b95c20a823c1268
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2019-03-12 12:18:09 -0700

    Add project files
    Add the xcode project files that will serve as the foundation for this project.

---
+++ Polaris/Polaris/Polaris.h
@@ -0,0 +1,19 @@
+//
+//  Polaris.h
+//  Polaris
+//
+//  Created by Andrew Despres on 3/12/19.
+//  Copyright © 2019 Downey City Library. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Polaris.
+FOUNDATION_EXPORT double PolarisVersionNumber;
+
+//! Project version string for Polaris.
+FOUNDATION_EXPORT const unsigned char PolarisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Polaris/PublicHeader.h>
+
+
