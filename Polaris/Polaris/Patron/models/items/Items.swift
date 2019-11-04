28afb2575abccacb90f5630bd331b5042b594e34
Andrew Despres <adespres@gmail.com>
2019-11-04 15:37:38 -0800

    Add out property
    Add the property "out" which uses a private method to calculate the unique items contained in all (referencing out).

--- Polaris/Polaris/Patron/models/items/Items.swift
+++ Polaris/Polaris/Patron/models/items/Items.swift
@@ -15,11 +15,13 @@ extension Patron {
         // MARK: - Private Properties (Get/Set)
         private var _all: [Item] = []
         private var _lost: [Item] = []
+        private var _out: [Item] { return uniqueItems(in: all, and: overdue) }
         private var _overdue: [Item] = []
         
         // MARK: - Public Properties (Get Only)
         public var all: [Item] { get { return _all } }
         public var lost: [Item] { get { return _lost } }
+        public var out: [Item] { get { return _out } }
         public var overdue: [Item] { get { return _overdue } }
         
         // MARK: - Public Setter
@@ -29,6 +31,15 @@ extension Patron {
             case .lost: _lost.append(contentsOf: items)
             case .overdue: _overdue.append(contentsOf: items)
             }
+        }
+        
+        // MARK: - Private Methods
+        private func uniqueItems(in a: [Item], and b: [Item]) -> [Item] {
+            let setA = Set(a)
+            let setB = Set(b)
+            let difference = setA.subtracting(setB)
+            
+            return Array(difference)
         }
     }
 }
