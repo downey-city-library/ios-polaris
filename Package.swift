5b0856f2217b87803273b289f775140f2b6aad86
Andrew Despres <adespres@gmail.com>
2022-06-27 17:31:10 -0700

    add macOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Polaris",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v11),
+        .macOS("12.0")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
