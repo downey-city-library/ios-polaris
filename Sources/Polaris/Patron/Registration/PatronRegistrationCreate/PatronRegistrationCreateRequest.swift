1295a2f35f07379e43b67cb4837ceed42baf4f97
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2023-10-04 15:46:19 -0700

    Modify property types and default values

--- Sources/Polaris/Patron/Registration/PatronRegistrationCreate/PatronRegistrationCreateRequest.swift
+++ Sources/Polaris/Patron/Registration/PatronRegistrationCreate/PatronRegistrationCreateRequest.swift
@@ -10,7 +10,7 @@ extension Polaris.Patron.Registration {
         public var workstation: Int
         public var patronBranch: Int
         
-        public var postalCode: Int = 0
+        public var postalCode: String = ""
         public var zipPlusFour: String = ""
         public var city: String = ""
         public var state: String = ""
@@ -31,9 +31,9 @@ extension Polaris.Patron.Registration {
         public var phoneVoice1: String = ""
         public var phoneVoice2: String = ""
         public var phoneVoice3: String = ""
-        public var phone1CarrierID: Int = 1
-        public var phone2CarrierID: Int = 0
-        public var phone3CarrierID: Int = 0
+        public var phone1CarrierID: Int?
+        public var phone2CarrierID: Int?
+        public var phone3CarrierID: Int?
         public var emailAddress: String = ""
         public var altEmailAddress: String = ""
         public var username: String = ""
@@ -42,10 +42,10 @@ extension Polaris.Patron.Registration {
         public var languageID: Int = 1
         public var deliveryOptionID: Int = 0
         public var enableSMS: Bool = false
-        public var txtPhoneNumber: Int = 0
+        public var txtPhoneNumber: Int?
         public var barcode: String = ""
-        public var eReceiptOptionID: Int = 0
-        public var patronCode: Int = 1
+        public var eReceiptOptionID: Int?
+        public var patronCode: Int
         public var expirationDate: String = ""
         public var addressCheckDate: String = ""
         
@@ -101,12 +101,88 @@ extension Polaris.Patron.Registration {
             branch: Int,
             user: Int,
             workstation: Int,
-            patronBranch: Int
+            patronBranch: Int,
+            postalCode: String,
+            zipPlusFour: String = "",
+            city: String,
+            state: String,
+            county: String,
+            countryID: Int,
+            streetOne: String,
+            streetTwo: String = "",
+            nameFirst: String,
+            nameLast: String,
+            nameMiddle: String = "",
+            user1: String = "",
+            user2: String = "",
+            user3: String = "",
+            user4: String = "",
+            user5: String = "",
+            gender: String,
+            birthdate: String,
+            phoneVoice1: String,
+            phoneVoice2: String = "",
+            phoneVoice3: String = "",
+            phone1CarrierID: Int?,
+            phone2CarrierID: Int? = nil,
+            phone3CarrierID: Int? = nil,
+            emailAddress: String,
+            altEmailAddress: String = "",
+            username: String = "",
+            password: String,
+            password2: String,
+            languageID: Int = 1,
+            deliveryOptionID: Int,
+            enableSMS: Bool,
+            txtPhoneNumber: Int?,
+            barcode: String,
+            eReceiptOptionID: Int?,
+            patronCode: Int,
+            expirationDate: String,
+            addressCheckDate: String
         ) {
             self.branch = branch
             self.user = user
             self.workstation = workstation
             self.patronBranch = patronBranch
+            self.postalCode = postalCode
+            self.zipPlusFour = zipPlusFour
+            self.city = city
+            self.state = state
+            self.county = county
+            self.countryID = countryID
+            self.streetOne = streetOne
+            self.streetTwo = streetTwo
+            self.nameFirst = nameFirst
+            self.nameLast = nameLast
+            self.nameMiddle = nameMiddle
+            self.user1 = user1
+            self.user2 = user2
+            self.user3 = user3
+            self.user4 = user4
+            self.user5 = user5
+            self.gender = gender
+            self.birthdate = birthdate
+            self.phoneVoice1 = phoneVoice1
+            self.phoneVoice2 = phoneVoice2
+            self.phoneVoice3 = phoneVoice3
+            self.phone1CarrierID = phone1CarrierID
+            self.phone2CarrierID = phone2CarrierID
+            self.phone3CarrierID = phone3CarrierID
+            self.emailAddress = emailAddress
+            self.altEmailAddress = altEmailAddress
+            self.username = username
+            self.password = password
+            self.password2 = password2
+            self.languageID = languageID
+            self.deliveryOptionID = deliveryOptionID
+            self.enableSMS = enableSMS
+            self.txtPhoneNumber = txtPhoneNumber
+            self.barcode = barcode
+            self.eReceiptOptionID = eReceiptOptionID
+            self.patronCode = patronCode
+            self.expirationDate = expirationDate
+            self.addressCheckDate = addressCheckDate
         }
     }
 }
